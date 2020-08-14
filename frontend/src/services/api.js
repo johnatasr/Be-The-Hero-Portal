45e2908501245f8397ee63e05cde9eb075a2d141
unknown <johnatasr@hotmail.com>
2020-08-14 19:31:31 -0300

    Add static admin

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -15,23 +15,30 @@ axiosInstance.interceptors.response.use(
       const originalRequest = error.config;
 
       if (error.response.status === 401 && error.response.statusText === "Unauthorized") {
-          const refresh_token = localStorage.getItem('refresh_token');
+            let refresh_token = localStorage.getItem('refresh_token');
+            let access_token = localStorage.getItem('access_token');
 
-          return axiosInstance
-              .post('core/token/refresh/', {refresh: refresh_token})
-              .then((response) => {
+          if ( (refresh_token == '' || refresh_token == undefined || refresh_token == null) && 
+                    ( access_token == '' || access_token == undefined || access_token == null ) ) {
+              return
+          }
+          else {
+            return axiosInstance
+            .post('core/token/refresh/', {refresh: refresh_token})
+            .then((response) => {
 
-                  localStorage.setItem('access_token', response.data.access);
-                  localStorage.setItem('refresh_token', response.data.refresh);
+                localStorage.setItem('access_token', response.data.access);
+                localStorage.setItem('refresh_token', response.data.refresh);
 
-                  axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access;
-                  originalRequest.headers['Authorization'] = "JWT " + response.data.access;
+                axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access;
+                originalRequest.headers['Authorization'] = "JWT " + response.data.access;
 
-                  return axiosInstance(originalRequest);
-              })
-              .catch(err => {
-                  console.log(err)
-              });
+                return axiosInstance(originalRequest);
+            })
+            .catch(err => {
+                console.log(err)
+            });
+          }    
       }
       return Promise.reject(error);
   }
