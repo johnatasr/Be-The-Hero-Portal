db70698b97124039e843868f6080bf5487670b9b
unknown <johnatasr@hotmail.com>
2020-08-14 00:12:32 -0300

    add PG, resolving fix

--- bethehero/settings.py
+++ bethehero/settings.py
@@ -87,15 +87,15 @@ WSGI_APPLICATION = 'bethehero.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES = {}
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 #
 
 # Password validation
