c9d0e7543582ba0c3e94249917f909562900751f
unknown <johnatasr@hotmail.com>
2020-08-14 10:19:46 -0300

    Add admin incidents

--- incidents/admin.py
+++ incidents/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from incidents.models import Incident
 
-# Register your models here.
+
+class IncidentsAdmin(admin.ModelAdmin):
+    fields = ['id', 'title', 'description', 'value', 'value_total', 'ong']
+    search_fields = ['id', 'title']
+
+    def get_readonly_fields(self, request, obj=None):
+        return self.readonly_fields + ('ong', 'value_total')
+
+admin.site.register(Incident, IncidentsAdmin)
+
--- ongs/admin.py
+++ ongs/admin.py
@@ -5,4 +5,7 @@ class OngsAdmin(admin.ModelAdmin):
     fields = ['id', 'name', 'email', 'whatsapp', 'city', 'uf', 'user']
     search_fields = ['id', 'name']
 
+    def get_readonly_fields(self, request, obj=None):
+        return self.readonly_fields + ('id', 'user')
+
 admin.site.register(Ong, OngsAdmin)
